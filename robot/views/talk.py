699d3a9b6c658ee8fa27334b36ec71106cbeee91
a6782533 <1311974687@qq.com>
2023-08-05 17:11:09 +0800

    更改客服头像

--- robot/views/talk.py
+++ robot/views/talk.py
@@ -8,9 +8,10 @@ def talk(request):
     info = request.session.get('info')
     user_id = info['id']
     query_set = UserInfo.objects.filter(id=user_id).first()
-    return render(request,'robot/talk.html',{'user_info': query_set,})
-def answer(request):
-    message='抱歉，您的问题我可能无法给出答复。请您把问题提的尽量准确一些。'
-    # 如果表中有了数据就报错
-    return JsonResponse({'status': True, 'err': "已经收藏",'message':message})
+    return render(request, 'robot/talk.html', {'user_info': query_set, })
 
+
+def answer(request):
+    message = '抱歉，您的问题我可能无法给出答复。请您把问题提的尽量准确一些。'
+    # 如果表中有了数据就报错
+    return JsonResponse({'status': True, 'err': "已经收藏", 'message': message})
