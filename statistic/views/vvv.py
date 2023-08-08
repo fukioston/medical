0285e7430dcdb033072ddfb95d24d81afb6122c4
Otonashi <2442669412@qq.com>
2023-08-08 20:35:51 +0800

    #修改

--- statistic/views/vvv.py
+++ statistic/views/vvv.py
@@ -1,5 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from medical.local_settings import g
+
 
 def statistic(request):
-    return render(request, 'statistic/sta.html')
+    graph = g
+    Disease = graph.run("MATCH(m:Disease) RETURN COUNT(m)").data()[0]['COUNT(m)']
+    Department = graph.run("MATCH(m:Department) RETURN COUNT(m)").data()[0]['COUNT(m)']
+    Drug = graph.run("MATCH(m:Drug) RETURN COUNT(m)").data()[0]['COUNT(m)']
+    Food = graph.run("MATCH(m:Food) RETURN COUNT(m)").data()[0]['COUNT(m)']
+    Symptom = graph.run("MATCH(m:Symptom) RETURN COUNT(m)").data()[0]['COUNT(m)']
+    li = [Disease, Department, Drug, Food, Symptom]
+    return render(request, 'statistic/sta.html', {'list': li, })
