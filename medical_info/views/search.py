1ed94de8b488bc116a133ddfb06022eadbbb987f
a6782533 <1311974687@qq.com>
2023-08-09 21:04:45 +0800

    医学信息搜索view demo

--- medical_info/views/search.py
+++ medical_info/views/search.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+from user.models import UserInfo
+
+
 def search(request):
-    return render(request, 'medical_info/search.html')
+    uinfo = request.session.get('info')
+    user_id = uinfo['id']
+    query_set = UserInfo.objects.filter(id=user_id).first()
+
+    kw = request.GET.get('kw')  # 获取参数值
+    # 用关键字搜索neo4j库
+    print(kw)
+
+    return render(request, 'medical_info/search.html', {'user_info': query_set})
